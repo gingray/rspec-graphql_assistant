b65d08ca8d12da326cfdf94078c239923849b635
Ivan Lopatin <gingray.dev@gmail.com>
2020-07-14 15:16:54 +0200

    initial commit

---
+++ lib/rspec/graphql_assistant.rb
@@ -0,0 +1,8 @@
+require "rspec/graphql_assistant/version"
+
+module Rspec
+  module GraphqlAssistant
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/rspec/graphql_assistant_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rspec::GraphqlAssistant do
+  it "has a version number" do
+    expect(Rspec::GraphqlAssistant::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
